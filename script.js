e71184a71045b445238c1e0895fccaec2bd7ae30
Whitney McGrew <wt.mcgrew@gmail.com>
2023-06-13 18:43:17 -0500

    Initial commit; already completed

---
+++ script.js
@@ -0,0 +1,27 @@
+let count = 0;
+
+document.getElementById("decrease").onclick = function() {
+	count -= 1;
+	document.getElementById("counterlabel").innerHTML = count;
+}
+
+document.getElementById("reset").onclick = function() {
+	count = 0;
+	document.getElementById("counterlabel").innerHTML = count;
+}
+
+document.getElementById("increase").onclick = function() {
+	count += 1;
+	document.getElementById("counterlabel").innerHTML = count;
+}
+
+/*
+
+Use DOM to target elements by ID on HTML document
+
+Onclick makes the buttons clickable on the HTML layout
+
+-= (count = count - 1)
++- (count = count + 1)
+
+*/
