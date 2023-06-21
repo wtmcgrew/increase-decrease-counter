6a30ae3aa73bceb6686b835e77b07d50ccb51697
Whitney McGrew <wt.mcgrew@gmail.com>
2023-06-20 19:43:32 -0500

    Refactor of JS code

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ let count = 0;
 
 document.getElementById("decrease").onclick = function() {
 	count -= 1;
-	document.getElementById("counterlabel").innerHTML = count;
+	document.getElementById("counter").innerHTML = count;
 }
 
 document.getElementById("reset").onclick = function() {
 	count = 0;
-	document.getElementById("counterlabel").innerHTML = count;
+	document.getElementById("counter").innerHTML = count;
 }
 
 document.getElementById("increase").onclick = function() {
 	count += 1;
-	document.getElementById("counterlabel").innerHTML = count;
+	document.getElementById("counter").innerHTML = count;
 }
 
 /*
